5243b449b545b651fb8972b0041c383a211635c7
fdrossii <fdrossi24@gmail.com>
2025-09-06 14:27:08 -0300

    1er Commit

---
+++ src/main/java/com/juliana/api_juliana/ApiJulianaApplication.java
@@ -0,0 +1,13 @@
+package com.juliana.api_juliana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJulianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJulianaApplication.class, args);
+	}
+
+}
