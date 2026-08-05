44a1d757a20736831bfca1e155e3dfec3a1fc623
fdrossii <fdrossi24@gmail.com>
2026-08-05 18:46:09 -0300

    Add exception logging

--- src/main/java/com/juliana/api_juliana/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/juliana/api_juliana/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.juliana.api_juliana.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleNotFound(ResourceNotFoundException e) {
@@ -29,6 +35,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGeneral(Exception e) {
+        log.error("Error interno del servidor: {}", String.valueOf(e));
         Map<String, Object> body = new HashMap<>();
         body.put("message", "Error interno del servidor");
         body.put("error", e.getMessage());
