53d9c8fb6a38b8274ecafbdb31e3e9cdc6384339
fdrossii <fdrossi24@gmail.com>
2026-08-10 20:57:33 -0300

    Test smtp

---
+++ src/main/java/com/juliana/api_juliana/controllers/STPMTestController.java
@@ -0,0 +1,25 @@
+package com.juliana.api_juliana.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+@RestController
+@RequestMapping("/api/test")
+public class STPMTestController {
+    @GetMapping("/test-smtp")
+    public String testSmtp() {
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("smtp.gmail.com", 587), 10000);
+            socket.close();
+
+            return "CONEXIÓN SMTP EXITOSA";
+        } catch (Exception e) {
+            return "ERROR SMTP: " + e.getClass().getName() + " - " + e.getMessage();
+        }
+    }
+}
